2f9bc5c0794d959093f33724f05782ecf8f78fc0
juhyeong park <majecty@gmail.com>
2015-07-03 07:43:41 +0000

    Clean up test.

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,9 +7,7 @@ describe message False = putStrLn $ "Fail test " ++ message
 should :: (Eq a) => a -> a -> Bool
 should = (==)
 
-distance = getDistance ((0, 0, 0), (10, 0, 0)) (0, 5, 0)
-
 main :: IO ()
 main = do
   describe "check distance" $
-    5.0 `should` distance
+    5.0 `should` getDistance ((0, 0, 0), (10, 0, 0)) (0, 5, 0)

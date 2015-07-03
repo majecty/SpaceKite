0502e3eaae1075bbc9a65a123cdb7e16f50b72b0
juhyeong park <majecty@gmail.com>
2015-07-03 07:42:57 +0000

    Make sample test util.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,15 @@
 import SpaceKite
 
+describe :: String -> Bool -> IO ()
+describe message True = putStrLn "."
+describe message False = putStrLn $ "Fail test " ++ message
+
+should :: (Eq a) => a -> a -> Bool
+should = (==)
+
 distance = getDistance ((0, 0, 0), (10, 0, 0)) (0, 5, 0)
 
 main :: IO ()
 main = do
-  putStrLn $ show distance
-  putStrLn "Test suite not yet implemented"
+  describe "check distance" $
+    5.0 `should` distance
